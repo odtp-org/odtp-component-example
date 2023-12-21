5b83c7462bb80966fcfa30ade88062e407370c0f
caviri <carlosvivarrios@gmail.com>
2023-12-21 23:54:01 +0100

    fixing compression of output

--- odtp-client/odtp-app.sh
+++ odtp-client/odtp-app.sh
@@ -12,7 +12,7 @@ if [ -v ODTP_MONGO_SERVER ]; then
     echo "STARTING LOGGING IN MONGO SERVER"
     python3 /odtp/odtp-client/logger.py >> /odtp/odtp-logs/odtpLoggerDebugging.txt 2>&1 &
 else
-    echo "ODTP_MONGO_URL does not exist"
+    echo "ODTP_MONGO_SERVER does not exist"
 fi
 
 
@@ -32,7 +32,7 @@ bash /odtp/odtp-app/app.sh
 
 #  Take output and export it
 cd /odtp/odtp-output
-zip -r /odtp/odtp-output/odtp-output.zip /odtp/odtp-output
+zip -r /odtp/odtp-output/odtp-output.zip /odtp/odtp-output/*
 
 #########################################################
 # ODTP SNAPSHOT CREATION 
